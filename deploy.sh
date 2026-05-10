08ff0bf1f035462b576aab47aa5cfbeaa03368f6
yigitsancar <ygtsncr07@gmail.com>
2026-05-10 23:37:35 +0300

    Fix existing container conflict

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,8 @@ sudo docker pull yigitsancar/publish-management-pro2:latest
 
 echo "Stopping old containers..."
 sudo docker compose -f /home/ubuntu/compose.yml down || true
-
+sudo docker rm -f publish-management-db || true
+sudo docker rm -f publish-management-app || true
 echo "Starting containers with compose..."
 sudo docker compose -f /home/ubuntu/compose.yml up -d
 
